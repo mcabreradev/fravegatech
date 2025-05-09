e596624fab9d23ecedcf75656659814ea69ce6cb
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2025-05-08 21:09:04 -0300

    Update Next.js configuration and add new dependencies for class variance and animations

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['avatars.githubusercontent.com', 'github.com', 'avatar-management--avatars.us-west-2.prod.public.atl-paas.net'],
+  },
+  swcMinify: true,
 };
 
 export default nextConfig;
